ba95fd1f0cb460c8610ff79a02b3ca0e97d5cb20
root <root@localhost.localdomain>
2021-03-24 12:11:51 -0400

    changed file name

---
+++ skeleton.ps1
@@ -0,0 +1,30 @@
+<# extra comment #>
+
+<# Overview
+this script will query the network for the name of the
+primary domain controller emulater and the domain, search AD,
+filter the output to display user accounts,
+and clean-up the output so it's easier to read.
+#>
+
+<# Components
+DirectorySearcher <--an object
+ldap <--a protocol
+#>
+
+#ldap provider path
+#LDAP://HostName[:PortNumber][/DistinguishedName]
+
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+
+$PDC = ($domainObj.PdcRoleOwner).Name
+
+$SearchString = "LDAP://"
+
+$SearchString += $PDC + "/"
+
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+
+$SearchString += $DistinguishedName
+
+$SearchString
