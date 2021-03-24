164ff2a7ff5abf53a048367a7931c39a0979babf
root <root@localhost.localdomain>
2021-03-24 14:12:46 -0400

    this branch's first commit

---
+++ get_spns.ps1
@@ -0,0 +1,44 @@
+<# Overview
+this script will query the network for the name of the
+primary domain controller emulater and the domain, search AD,
+filter the output to display user accounts,
+and clean-up the output so it's easier to read.
+#>
+
+<# Components
+DirectorySearcher <--an object
+ldap <--a protocol
+#>
+
+#ldap provider path
+#LDAP://HostName[:PortNumber][/DistinguishedName]
+
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+
+$PDC = ($domainObj.PdcRoleOwner).Name
+
+$SearchString = "LDAP://"
+
+$SearchString += $PDC + "/"
+
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+
+$SearchString += $DistinguishedName
+
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+
+$Searcher.SearchRoot = $objDomain
+
+$Searcher.filter="serviceprincipalname=*http*"
+
+$Result = $Searcher.FindAll()
+
+Foreach($obj in $Result)
+{
+	Foreach($prop in $obj.Properties)
+	{
+		$prop
+	}
+}
