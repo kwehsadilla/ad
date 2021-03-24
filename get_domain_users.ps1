a33c2a541e35f2cab7da2877489d06c891934ff3
root <root@localhost.localdomain>
2021-03-24 11:35:19 -0400

    enumerate all users in the domain

---
+++ get_domain_users.ps1
@@ -0,0 +1,42 @@
+<# extra comment #>
+
+<# Overview
+this script will query the network for the name of the
+primary domain controller emulater and the domain, search AD,
+filter the output to display user accounts,
+and clean-up the output so it's easier to read.
+#>
+
+<# Components
+DirectorySearcher <--an object
+ldap <--a protocol
+#>
+
+#ldap provider path
+#LDAP://HostName[:PortNumber][/DistinguishedName]
+
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+
+$PDC = ($domainObj.PdcRoleOwner).Name
+
+$SearchString = "LDAP://"
+
+$SearchString += $PDC + "/"
+
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+
+$SearchString += $DistinguishedName
+
+$SearchString
+
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+
+$Searcher.SearchRoot = $objDomain
+
+<# FILTERS #>
+
+$Searcher.filter = "samAccountType=805306368"
+
+$Searcher.FindAll()
