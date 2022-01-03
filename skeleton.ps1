eb36b6fe93c189577e5ae83ca85e68dfb3331208
kwehsadilla <>
2022-01-03 11:50:17 +0100

    fixed typo

--- skeleton.ps1
+++ skeleton.ps1
@@ -1,6 +1,6 @@
 <# Overview
 this script will query the network for the name of the
-primary domain controller emulater and the domain, search AD,
+primary domain controller emulator and the domain, search AD,
 filter the output to display user accounts,
 and clean-up the output so it's easier to read.
 #>
